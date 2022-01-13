9e4acb1157c652c5d7870eb2ad0e8ccdcb1954f0
jisoooit <ydsn336@naver.com>
2022-01-13 22:52:47 +0900

    binary

---
+++ Basic/src/Binary.java
@@ -0,0 +1,23 @@
+
+public class Binary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		int bnum=0B1010;
+		int onum=012;
+		int hnum=0XA;
+		int num1=0B00000000000000000000000000000101;
+		int num2=0B11111111111111111111111111111011;
+		
+		System.out.println(num);
+		System.out.println(bnum);
+		System.out.println(onum);
+		System.out.println(hnum);
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+	}
+
+}
